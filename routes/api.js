e3d1743988e8f47f4ed9332ae2d674457e278350
Sander Sierra <sierrasander29@gmail.com>
2023-01-30 18:53:40 -0500

    fix: message error for invalidad number and unit

--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,9 @@ module.exports = function (app) {
     const initNum = convertHandler.getNum(input)
     const initUnit = convertHandler.getUnit(input)
 
-    if (!initUnit && !initNum)
+    console.log(initNum, initUnit)
+
+    if (!(initUnit && initNum))
       return res.send("invalid number and unit")
     else if (!initUnit)
       return res.send("invalid unit")
